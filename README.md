49165fbff54a53187a2db6528f9b01edc5f0bdc6
Joseph C <neckro@gmail.com>
2015-01-11 11:01:39 -0600

    First commit; README and frasier.py

---
+++ README.md
@@ -0,0 +1,7 @@
+Generator for Frasier quotes.  *All* of the Frasier quotes.
+
+Usage: `frasier.py <characters>`, defaults to "Frasier".  This is a substring
+match.  For example: `frasier.py niles daphne`
+
+Scripts were stolen from the "Frasier Script Archive" here:
+http://catsnguitars.wordpress.com/scripts-archive/
---
+++ frasier.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python2
+# coding=utf-8
+
+import fileinput
+import sys
+from re import match, sub
+from glob import glob
+
+
+def parse(input, characters=[], prefix_speaker=False):
+    if len(characters) == 0:
+        characters = ['Frasier']
+    characters = map(str.lower, characters)
+    parse = False
+    out = ""
+    for line in input:
+        if len(line.strip()) == 0:
+            continue
+        result = match(r"^(\s*)(.+?)\: (.*?)(\s*)$", line)
+        if result:
+            speaker = result.group(2).lower()
+            text = result.group(3)
+            if any(c.lower() in speaker for c in characters):
+                parse = True
+                print_out(out)
+                if prefix_speaker:
+                    out = speaker + ': ' + text
+                else:
+                    out = text
+                continue
+        if parse:
+            result = match(r"^ {8}(.+?)$", line)
+            if result:
+                text = result.group(1).strip()
+                if text[len(text)-1] != u"-":
+                    out += u" "
+                out += text
+            else:
+                parse = False
+    if len(out) > 0:
+        print_out(out)
+
+
+def print_out(out):
+    out = sub(r"( *)\[.+\]( *)", " ", out)
+    out = sub(r" +", " ", out).strip()
+    if len(out) > 0:
+        out = unsmarten(out)
+        print out.encode("utf-8")
+
+
+def unsmarten(text):
+    # smart single quotes: ‘’
+    text = text.replace(u"\u2018", "'").replace(u"\u2019", "'")
+    # smart double quotes: “”
+    text = text.replace(u"\u201c", '"').replace(u"\u201d", '"')
+    # dashes: –
+    text = text.replace(u"\u2013", '-')
+    return text
+
+if __name__ == "__main__":
+    input = fileinput.FileInput(
+        files=glob("scripts/*.txt"),
+        openhook=fileinput.hook_encoded("utf8")
+    )
+    parse(input, sys.argv[1:])
